4e8bcc9f72b04a2ef1879cdb50a9876c18c4c219
Krisrina Shtyrkhatskaya <shtyrhackaya@yandex.ru>
2023-04-06 22:14:58 +0700

    Создание проекта

---
+++ page/index.js
@@ -0,0 +1,26 @@
+const openPopupBtn = document.querySelector('.profile__name_popup-open');
+const popup = document.querySelector('.popup');
+const closePopupBtn = document.querySelector('.popup__close');
+
+let formElement = document.querySelector('.form');
+let nameInput = document.querySelector('.form__text-name');
+let jobInput = document.querySelector('.form__text-about');
+let profileName = document.querySelector('.profile__name');
+let profileAbout = document.querySelector('.profile__about');
+
+function toglePopup () {
+    popup.classList.toggle('popup_opened');
+    nameInput.value = profileName.textContent;
+    jobInput.value = profileAbout.textContent;
+}
+
+function handleFormSubmit (evt) {
+    evt.preventDefault(); 
+    profileName.textContent = nameInput.value;
+    profileAbout.textContent = jobInput.value;
+    toglePopup ()
+}
+
+formElement.addEventListener('submit', handleFormSubmit); 
+openPopupBtn.addEventListener('click', toglePopup);
+closePopupBtn.addEventListener('click', toglePopup);
