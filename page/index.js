10f835c3d10fa7dd74c5d544a924d94e6f4a614f
Krisrina Shtyrkhatskaya <shtyrhackaya@yandex.ru>
2023-04-06 23:50:22 +0700

    _

--- page/index.js
+++ page/index.js
@@ -1,4 +1,4 @@
-const openPopupBtn = document.querySelector('.profile__name_open');
+const openPopupBtn = document.querySelector('.profile__name__open');
 const popup = document.querySelector('.popup');
 const closePopupBtn = document.querySelector('.popup__close');
 
