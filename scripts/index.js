876d5cdf4ad54fba3ffde3dfeaa1b5d699b78d86
Krisrina Shtyrkhatskaya <shtyrhackaya@yandex.ru>
2023-04-27 00:12:45 +0700

    Исправления БЭМ

--- scripts/index.js
+++ scripts/index.js
@@ -127,8 +127,8 @@ const createData = (elementData) => {
 
  // Открытие попапа с картинкой
  const popupImg = document.querySelector('.popup-img');
- const openPictureLink = document.querySelector('.popup-img__figure_type_photo-open');
- const openPictureName = document.querySelector('.popup-img__figure_type_place-open');
+ const openPictureLink = document.querySelector('.popup-img__figure_photo');
+ const openPictureName = document.querySelector('.popup-img__figure_place');
 
  function openPicture () {
    popupImg.classList.add('popup-img_open');
